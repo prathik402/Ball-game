9f141b7a9e5d65345653faa962c8860ccab8b119
prathik402 <prathik402@gmail.com>
2021-03-27 10:27:29 +0530

    ballgame

--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
 var ball;
-
+var database;
+var position;
 function setup(){
     createCanvas(500,500);
+    database = firebase.database();
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref("ball/position");
+    ballPosition.on("value",readPosition,showError);
 }
 
 function draw(){
@@ -24,6 +28,16 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+   database.ref("ball/position").set({
+       "x": position.x + x,
+       "y": position.y + y
+   })
+}
+function readPosition(data) {
+    position = data.val()
+    ball.x = position.x 
+    ball.y = position.y
+}
+function showError() {
+console.log("errorInWritingInDataBase")
 }
